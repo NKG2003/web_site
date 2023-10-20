266bc68ae1819676d33c89171de5edeefd5fdcca
NKG <kaliollanurbek14@gmail.com>
2023-10-20 16:12:32 +0600

    2 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,35 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-  fontFamily: {
+    fontFamily: {
+
+
+      // extend: {
+      //   backgroundImage: {
+      //     'hero-pattern': "url('/img/office.jpeg')",
+          
+      //   }
+      // },
+
         roboto: ['Roboto Condensed', 'sans-serif'],
 
-        screens: {
-          'sm': '640px',
-          // => @media (min-width: 640px) { ... }
-    
-          'md': '768px',
-          // => @media (min-width: 768px) { ... }
-    
-          'lg': '1300px',
-          // => @media (min-width: 1024px) { ... }
-    
-          'xl': '1280px',
-          // => @media (min-width: 1280px) { ... }
-    
-          '2xl': '1536px',
-          // => @media (min-width: 1536px) { ... }
-        }
-     
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1300px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+
     },
   },
   plugins: [],
